72d85709e84e6eb1b8c3ed7b91a272bf7f078939
D3dde <daniel.lazza@tiscali.it>
2024-09-20 21:38:01 +0200

    final commit

--- src/auto/Main.java
+++ src/auto/Main.java
@@ -16,15 +16,34 @@ public class Main {
                     "5 - modifica dati di un auto\n" +
                     "6 - visualizza le auto in ordine crescente\n" +
                     "7 - visualizza tutti i modelli diesel\n" +
-                    "lascia il campo vuoto per terminare il programma");
+                    "0 - termina il programma");
             switch(in.nextInt()){
                 case 1:
                     System.out.println("inserisci in ordine : marca (INVIO) modello (INVIO) prezzo (INVIO)");
-                    Metodi.aggiuntaAuto(in.next(),in.next(),in.next());
+                    Metodi.aggiuntaAuto(in.next(),in.next(),in.nextInt());
                     break;
                 case 2:
                     System.out.println(Metodi.stampaAuto());
                     break;
+                case 3:
+                    System.out.println("inserisci in ordine : marca (INVIO) modello (INVIO)");
+                    System.out.println(Metodi.trovaAuto(in.next(),in.next()));
+                    break;
+                case 4:
+                    System.out.println("inserisci in ordine : marca (INVIO) modello (INVIO)");
+                    Metodi.cancellaAuto(in.next(),in.next());
+                    break;
+                case 5:
+                    System.out.println("inserisci in ordine i dati dell'auto da modificare: marca (INVIO) modello (INVIO)\npoi inserisci le modifiche: marca (INVIO) modello (INVIO) prezzo (INVIO)");
+                    Metodi.editAuto(in.next(),in.next(),in.next(),in.next(),in.nextInt());
+                    break;
+                case 6:
+                    System.out.println(Metodi.stampaSortPrezzo());
+                    break;
+                case 7:
+                    System.out.println(Metodi.filterDiesel());
+                    break;
+                case 0:
                 default:
                     ripeti=false;
             }
--- src/auto/Metodi.java
+++ src/auto/Metodi.java
@@ -1,11 +1,11 @@
 package auto;
 
 public class Metodi {
-    private static String[] prezzo=new String[100];
+    private static int[] prezzo=new int[100];
     private static String[] marca=new String[100];
     private static String[] modello=new String[100];
 
-    public static void aggiuntaAuto(String marcaNuova, String modelloNuovo, String prezzoNuovo){
+    public static void aggiuntaAuto(String marcaNuova, String modelloNuovo, int prezzoNuovo){
         for (int i = 0; i < marca.length; i++){
             if (marca[i]==null){
                 marca[i]=marcaNuova;
@@ -22,6 +22,82 @@ public class Metodi {
                 break;
             }
             out+= marca[i] + " - " + modello[i] + " - " + prezzo[i] +"\n";
+        }
+        return out;
+    }
+    public static String trovaAuto(String findMarca, String findModello){
+        String out = "";
+        for (int i = 0; i< marca.length; i++){
+            if (findMarca.equalsIgnoreCase(marca[i]) && findModello.equalsIgnoreCase(modello[i])){
+                out+= marca[i] + " - " + modello[i] + " - " + prezzo[i] +"\n";
+            }
+            if (marca[i]==null){
+                break;
+            }
+        }
+        return out;
+    }
+    public static void cancellaAuto(String deleteMarca, String deleteModello){
+        for (int i = 0; i< marca.length; i++){
+            if (marca[i]==null){
+                break;
+            }
+            if (deleteMarca.equalsIgnoreCase(marca[i]) && deleteModello.equalsIgnoreCase(modello[i])){
+                for (int j=i; j<marca.length-1; j++){
+                    if (marca[j]==null){
+                        break;
+                    }
+                    marca[j]=marca[j+1];
+                    modello[j]=modello[j+1];
+                    prezzo[j]=prezzo[j+1];
+
+                }
+            }
+        }
+    }
+    public static void editAuto(String findMarca, String findModello, String editMarca, String editModello, int editPrezzo){
+        for (int i = 0; i< marca.length; i++){
+            if (findMarca.equalsIgnoreCase(marca[i]) && findModello.equalsIgnoreCase(modello[i])){
+                marca[i]=editMarca;
+                modello[i]=editModello;
+                prezzo[i]=editPrezzo;
+            }
+            if (marca[i]==null){
+                break;
+            }
+        }
+    }
+    public static String stampaSortPrezzo(){
+        for (int i = 0; i < marca.length - 1; i++) {
+            int minimo = i;
+            for (int j = i + 1; j < marca.length; j++) {
+                if (prezzo[j] < prezzo[minimo] && prezzo[j]!=0)
+                    minimo = j;
+            }
+            int temp = prezzo[minimo];
+            prezzo[minimo] = prezzo[i];
+            prezzo[i] = temp;
+            String temp1 = marca[minimo];
+            marca[minimo] = marca[i];
+            marca[i] = temp1;
+            String temp2 = modello[minimo];
+            modello[minimo] = modello[i];
+            modello[i] = temp2;
+            if (marca[i]==null){
+                break;
+            }
+        }
+        return stampaAuto();
+    }
+    public static String filterDiesel(){
+        String out = "";
+        for (int i = 0; i < marca.length; i++){
+            if (marca[i]==null){
+                break;
+            }
+            if (modello[i].equalsIgnoreCase("diesel")){
+                out+= marca[i] + " - " + modello[i] + " - " + prezzo[i] +"\n";
+            }
         }
         return out;
     }
